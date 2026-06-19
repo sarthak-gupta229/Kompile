58572cb3796a8a9c33034d005fa8d299832c877f
Sarthak Gupta <guptasarthak229@gmail.com>
2026-06-19 15:34:50 +0530

    feat: created userSchema (#26)
    * feat: implement healthcheck endpoint using router and controller architecture
    
    * feat: created userSchema

---
+++ server/src/models/user.models.js
@@ -0,0 +1,110 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    //auth part
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placehold.co/200x200`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+    //data
+    bio: {
+      type: String,
+      maxLength: [200, "Bio cannot exceed 200 characters"],
+      trim: true,
+    },
+    country: {
+      type: String,
+      trim: true,
+    },
+    techStack: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    education: {
+      institution: { type: String, trim: true },
+      degree: { type: String, trim: true },
+      branch: { type: String, trim: true },
+      graduationYear: { type: Number },
+    },
+    socialLinks: {
+      linkedin: { type: String, trim: true },
+      twitter: { type: String, trim: true },
+      website: { type: String, trim: true },
+      resume: { type: String, trim: true },
+    },
+    // platform data
+    connectedPlatforms: [
+      {
+        platform: {
+          type: String,
+          enum: ["leetcode", "github", "codeforces", "hackerrank", "gfg"],
+          required: true,
+        },
+        username: { type: String, trim: true, required: true },
+        isVerified: { type: Boolean, default: false },
+        connectedAt: { type: Date, default: Date.now },
+      },
+    ],
+    stats: {
+      totalQuestions: { type: Number, default: 0 },
+      totalActiveDays: { type: Number, default: 0 },
+      lastSyncedAt: { type: Date },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
