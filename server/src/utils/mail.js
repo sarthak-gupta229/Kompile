1a7f34b27fe7d3d2145adeb8359daf41d17b0fb6
Sarthak Gupta <guptasarthak229@gmail.com>
2026-06-20 16:54:11 +0530

    feat: effect (#28)
    * feat: implement healthcheck endpoint using router and controller architecture
    
    * feat: created userSchema
    
    * feat: add MacbookScroll component,

---
+++ server/src/utils/mail.js
@@ -0,0 +1,19 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+// const sendEmail = async (options) => {
+//   const mailGenerator = new Mailgen({
+//     theme: "default",
+//     product: {
+//       name: "Kompile",
+//       link: "https://Kompilee.com",
+//     },
+//   });
+//   const emailBody = mailGenerator.generatePlaintext(options.mailgenContent);
+//   const emailHtml= mailGenerator.generate(options.mailgenContent);
+
+
+// };
+
+
+con
