7771ef2924ee4f4c2e1e17c45ffeb03d0f6a01e1
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-08-27 19:51:38 +0530

    refactor: migrate from native node env-file flag to dotenv package for environment variable management

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,6 @@
+import "dotenv/config";
 import app from "./app.js";
-import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-dotenv.config({
-  path: "./.env",
-});
 
 const PORT = process.env.PORT || 8000;
 
